4caabda4013a323ef893b713c416a5562164d24e
Jeanina Lao <lao13001@byui.edu>
2018-12-10 11:26:48 -0700

    part 1 of tutorial done

---
+++ Lao_ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lao_ContosoUniversity.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lao_ContosoUniversity.Models.Student> Student { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+        public DbSet<Course> Course { get; set; }
+    }
+}
--- Lao_ContosoUniversity/Program.cs
+++ Lao_ContosoUniversity/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Lao_ContosoUniversity.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Lao_ContosoUniversity.Data;
 
 namespace Lao_ContosoUniversity
 {
@@ -14,7 +17,28 @@ namespace Lao_ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    // context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
+
+                CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
